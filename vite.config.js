f9d762e9852d7daa4bb211e0e8000415cfef6dcc
Wietse Venema <356014+wietsevenema@users.noreply.github.com>
2025-11-08 12:14:04 +0100

    feat: integrate adventure-ui subtree and configure build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    base: '/play/',
     plugins: [react()],
     server: {
       proxy: {
